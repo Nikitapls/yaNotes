32e463f11bf0097af6e9950888e7aa5505232008
Nikita <nikitanikpls@gmail.com>
2020-02-19 18:18:23 +0300

    Initial Commit

---
+++ NotesList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotesList
+//
+//  Created by ios_school on 2/19/20.
+//  Copyright © 2020 ios_school. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
