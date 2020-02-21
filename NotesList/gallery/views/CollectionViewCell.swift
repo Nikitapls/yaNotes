72847d5f19152d269af30afb987871b9c074aa25
Nikita <nikitanikpls@gmail.com>
2020-02-21 13:27:27 +0300

    //

--- NotesList/gallery/views/CollectionViewCell.swift
+++ NotesList/gallery/views/CollectionViewCell.swift
@@ -10,7 +10,5 @@ import UIKit
 
 class CollectionViewCell: UICollectionViewCell {
      @IBOutlet weak var label: UILabel!
-    override func awakeFromNib() {
-      super.awakeFromNib()
-    }
+    
 }
