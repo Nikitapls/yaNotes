3181f24df7b67a6cfedd33201e43130a5e8151fc
Nikita <nikitanikpls@gmail.com>
2020-02-21 16:44:27 +0300

    with camera

--- NotesList/gallery/controllers/MainViewController.swift
+++ NotesList/gallery/controllers/MainViewController.swift
@@ -12,28 +12,93 @@ class MainViewController: UIViewController {
     
     var photos = Photo.allPhotos()
     @IBOutlet weak var collectionView: UICollectionView!
-    
+    var imagPickUp : UIImagePickerController!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.dataSource = self
         collectionView.delegate = self
+        imagPickUp = self.imageAndVideos()
     }
     
     @IBAction func nextButtonClicked(_ sender: UIBarButtonItem) {
+        buttonClicked()
     }
     
-    /*
-    // MARK: - Navigation
+    @objc func buttonClicked() {
+        let ActionSheet = UIAlertController(title: nil, message: "Select Photo", preferredStyle: .actionSheet)
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        let cameraPhoto = UIAlertAction(title: "Camera", style: .default, handler: {
+            (alert: UIAlertAction) -> Void in
+            if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera){
+
+                self.imagPickUp.mediaTypes = ["public.image"]
+                self.imagPickUp.sourceType = UIImagePickerController.SourceType.camera;
+                self.present(self.imagPickUp, animated: true, completion: nil)
+            }
+            else{
+                UIAlertController(title: "iOSDevCenter", message: "No Camera available.", preferredStyle: .alert).show(self, sender: nil);
+            }
+
+        })
+
+        let PhotoLibrary = UIAlertAction(title: "Photo Library", style: .default, handler: {
+            (alert: UIAlertAction) -> Void in
+            if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.photoLibrary){
+                self.imagPickUp.mediaTypes = ["public.image"]
+                self.imagPickUp.sourceType = UIImagePickerController.SourceType.photoLibrary;
+                self.present(self.imagPickUp, animated: true, completion: nil)
+            }
+
+        })
+
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: {
+            (alert: UIAlertAction) -> Void in
+
+        })
+
+        ActionSheet.addAction(cameraPhoto)
+        ActionSheet.addAction(PhotoLibrary)
+        ActionSheet.addAction(cancelAction)
+
+
+        if UIDevice.current.userInterfaceIdiom == .pad{
+            let presentC : UIPopoverPresentationController  = ActionSheet.popoverPresentationController!
+            presentC.sourceView = self.view
+            presentC.sourceRect = self.view.bounds
+            self.present(ActionSheet, animated: true, completion: nil)
+        }
+        else{
+            self.present(ActionSheet, animated: true, completion: nil)
+        }
     }
-    */
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else { return }
+        photos.append(Photo(image: image))
+        imagPickUp.dismiss(animated: true, completion: { () -> Void in
+            // Dismiss
+        })
 
+    }
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        imagPickUp.dismiss(animated: true, completion: { () -> Void in
+            // Dismiss
+        })
+    }
+}
+
+extension MainViewController: UIImagePickerControllerDelegate , UINavigationControllerDelegate {
+    func imageAndVideos()-> UIImagePickerController{
+        if(imagPickUp == nil){
+            imagPickUp = UIImagePickerController()
+            imagPickUp.delegate = self
+            imagPickUp.allowsEditing = false
+        }
+        return imagPickUp
+    }
 }
 
 extension MainViewController: UICollectionViewDataSource, UICollectionViewDelegate {
