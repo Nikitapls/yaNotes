0843611360c674aa1b12f51879cc699393359f3c
Nikita <nikitanikpls@gmail.com>
2020-03-08 21:22:22 +0300

    //

--- NotesList/Operations/BackendOpertations/LoadNotesBackendOperation.swift
+++ NotesList/Operations/BackendOpertations/LoadNotesBackendOperation.swift
@@ -30,7 +30,7 @@ class LoadNotesBackendOperation: BaseBackendOperation {
     
     func updateData() {
         let stringUrl = "https://api.github.com/gists"
-        let fileName = "ios-course-notes-db"
+//        let fileName = "ios-course-notes-db"
         //let token = "26151f23b63e588415729feb76658d125e61075d"
         let components = URLComponents(string: stringUrl)
         guard let url = components?.url else { return }
@@ -40,17 +40,14 @@ class LoadNotesBackendOperation: BaseBackendOperation {
         request.httpMethod = "GET"
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
             guard let data = data else { return }
-//            let str = String(decoding: data, as: UTF8.self)
-//            print(str)
             guard let gistArr = try? JSONDecoder().decode([GistDownload].self, from: data) else {
                 print("Error while parsing data")
                 return
             }
-            print(gistArr)
             guard let index = gistArr.firstIndex(where: {
                 if ($0.files.firstIndex(where: {
                     print($0.key)
-                    if $0.key == fileName {
+                    if $0.key == self.fileName {
                         return true
                     }
                     return false
@@ -58,9 +55,19 @@ class LoadNotesBackendOperation: BaseBackendOperation {
                     return true
                 } else { return false }
             }) else { print("no");return }
-            print(gistArr[index])
+            //print(gistArr[index].files[self.fileName]?.rawUrl)
+            self.notesFromGistDownload(gist: gistArr[index])
         }
         task.resume()
     }
     
+    func notesFromGistDownload(gist: GistDownload) {
+        guard let urlPath = gist.files[self.fileName]?.rawUrl,
+            let url = URL(string: urlPath) else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+        }
+    }
 }
